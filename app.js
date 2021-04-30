89d5ecf09d13f9bd740cd743ea11734a7735eb3e
robertov92 <vil13004@byui.edu>
2021-04-30 14:05:36 -0500

    complete activity

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-var PORT = process.env.PORT || 3000;
-
 const app = express();
+
+const books = [];
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -15,11 +15,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //routes
 app.post('/add-book', (req, res, next) => {
+    books.push({ title: req.body.title, summary: req.body.summary })
     res.render('display-book', { title: req.body.title, summary: req.body.summary });
 });
 
-app.get('/', (req, res, next) => {
-    res.render('index');
+app.post('/remove-book', (req, res, next) => {
+    books.splice(req.body.deleteBook, 1)
+    res.redirect('/');
 });
 
+app.get('/', (req, res, next) => {
+    res.render('index', { books: books });
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
