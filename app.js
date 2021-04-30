bc7a31a22fe475a27037e910bddaa251f17fb011
robertov92 <vil13004@byui.edu>
2021-04-30 11:11:02 -0500

    fixing port and start method

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 3000;
 
 const app = express();
 
